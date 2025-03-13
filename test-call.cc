d0b5b9b3edb151ce02e4758849fc234c16dbbc8b
gaomingyu <874774259@qq.com>
2025-03-13 17:46:18 +0800

    test-call

--- test-call.cc
+++ test-call.cc
@@ -4,12 +4,21 @@
 
 int main()
 {
-    auto a = dlLoadLibrary("user32.dll");
-    if (a)
+    if (const auto a = dlLoadLibrary("user32.dll"))
     {
-        auto b = dlFindSymbol(a, "MessageBoxW");
-        if (b)
+        if (const auto b = dlFindSymbol(a, "MessageBoxW"))
         {
+            if (const auto vm = dcNewCallVM(4096))
+            {
+                dcMode(vm, DC_CALL_C_X86_WIN32_STD);
+                dcReset(vm);
+                dcArgPointer(vm, nullptr);
+                dcArgPointer(vm, (DCpointer)L"asd");
+                dcArgPointer(vm, (DCpointer)L"qwe");
+                dcArgInt(vm, 0);
+                const auto c = dcCallInt(vm, b);
+                dcFree(vm);
+            }
         }
         dlFreeLibrary(a);
     }
