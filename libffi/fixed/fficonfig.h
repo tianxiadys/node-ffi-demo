b7abb168921bc1e763944fb76b97b386ee2bf11d
gaomingyu <874774259@qq.com>
2025-03-23 00:33:39 +0800

    config

--- libffi/fixed/fficonfig.h
+++ libffi/fixed/fficonfig.h
@@ -5,117 +5,13 @@
 #ifndef LIBFFI_FIXED_CONFIG_H
 #define LIBFFI_FIXED_CONFIG_H
 
-// 确定没用
-// /* Define to the sub-directory where libtool stores uninstalled libraries. */
-// #define LT_OBJDIR ".libs/"
-// /* Name of package */
-// #define PACKAGE "libffi"
-// /* Define to the address where bug reports for this package should be sent. */
-// #define PACKAGE_BUGREPORT "http://github.com/libffi/libffi/issues"
-// /* Define to the full name of this package. */
-// #define PACKAGE_NAME "libffi"
-// /* Define to the full name and version of this package. */
-// #define PACKAGE_STRING "libffi 3.4.7"
-// /* Define to the one symbol short name of this package. */
-// #define PACKAGE_TARNAME "libffi"
-// /* Define to the home page for this package. */
-// #define PACKAGE_URL ""
-// /* Define to the version of this package. */
-// #define PACKAGE_VERSION "3.4.7"
-// /* The size of `double', as computed by sizeof. */
-// #define SIZEOF_DOUBLE 8
-// /* The size of `long double', as computed by sizeof. */
-// #define SIZEOF_LONG_DOUBLE 16
-// /* The size of `size_t', as computed by sizeof. */
-// #define SIZEOF_SIZE_T 8
-// /* Define to 1 if GNU symbol versioning is used for libatomic. */
-// /* #undef LIBFFI_GNU_SYMBOL_VERSIONING */
-// /* Version number of package */
-// #define VERSION "3.4.7"
-// /* Define to 1 if you have the <stdint.h> header file. */
-// #define HAVE_STDINT_H 1
-// /* Define to 1 if you have the <stdio.h> header file. */
-// #define HAVE_STDIO_H 1
-// /* Define to 1 if you have the <stdlib.h> header file. */
-// #define HAVE_STDLIB_H 1
-// /* Define to 1 if you have the <string.h> header file. */
-// #define HAVE_STRING_H 1
-// /* Define to 1 if you have the <sys/stat.h> header file. */
-// #define HAVE_SYS_STAT_H 1
-// /* Define to 1 if you have the <sys/types.h> header file. */
-// #define HAVE_SYS_TYPES_H 1
-// /* Define to 1 if you have the <strings.h> header file. */
-// #define HAVE_STRINGS_H 1
-// /* Define if your assembler and linker support unaligned PC relative relocs.
-//    */
-// /* #undef HAVE_AS_SPARC_UA_PCREL */
-// /* Define to 1 if you have the <inttypes.h> header file. */
-// #define HAVE_INTTYPES_H 1
-// /* Define if your assembler supports .register. */
-// /* #undef HAVE_AS_REGISTER_PSEUDO_OP */
-// /* Define if symbols are underscored. */
-// /* #undef SYMBOL_UNDERSCORE */
-// /* Define this if you are using Purify and want to suppress spurious messages.
-//    */
-// /* #undef USING_PURIFY */
-//
-// 间接没用
-// /* Define to 1 if you have the `memcpy' function. */
-// #define HAVE_MEMCPY 1
-// /* Define if building universal (internal helper macro) */
-// /* #undef AC_APPLE_UNIVERSAL_BUILD */
-// /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
-//    significant byte first (like Motorola and SPARC, unlike Intel). */
-// #if defined AC_APPLE_UNIVERSAL_BUILD
-// # if defined __BIG_ENDIAN__
-// #  define WORDS_BIGENDIAN 1
-// # endif
-// #else
-// # ifndef WORDS_BIGENDIAN
-// /* #  undef WORDS_BIGENDIAN */
-// # endif
-// #endif
-// /* Define this if you do not want support for aggregate types. */
-// /* #undef FFI_NO_STRUCTS */
-//
-// 无人支持
-// /* Define if you support more than one size of the long double type */
-// /* #undef HAVE_LONG_DOUBLE_VARIANT */
-// /* Define to 1 if you have the <sys/memfd.h> header file. */
-// /* #undef HAVE_SYS_MEMFD_H */
-// 没实际作用
-// #if !(defined __APPLE__ && defined __aarch64__)
-// /* Define if you have the long double type and it is bigger than a double */
-// #define HAVE_LONG_DOUBLE 1
-// #endif
-
-
-//暂时用不到
-/* Define this if you do not want support for the raw API. */
 #define FFI_NO_RAW_API 1
-/* Define this if you want extra debugging. */
-#undef FFI_DEBUG
-
-
-//所有支持的平台都有标准头
-/* Define to 1 if all of the C90 standard headers exist (not just the ones
-   required in a freestanding environment). This macro is provided for
-   backward compatibility; new code need not use it. */
 #define STDC_HEADERS 1
 
-
-#if !defined _MSC_VER
-/* Define if __attribute__((visibility("hidden"))) is supported. */
-#define HAVE_HIDDEN_VISIBILITY_ATTRIBUTE 1
-#endif
-
-#if !defined _WIN32
-/* Define to 1 if you have the <alloca.h> header file. */
-#define HAVE_ALLOCA_H 1
-/* Define to 1 if you have the <unistd.h> header file. */
-#define HAVE_UNISTD_H 1
-/* Define to 1 if you have the <dlfcn.h> header file. */
-#define HAVE_DLFCN_H 1
+#ifndef _WIN32
+# define HAVE_ALLOCA_H 1
+# define HAVE_DLFCN_H 1
+# define HAVE_UNISTD_H 1
 #endif
 
 #if defined __linux__
@@ -124,35 +20,30 @@
 /* Define to 1 if you have the `memfd_create' function. */
 #define HAVE_MEMFD_CREATE 1
 #endif
-
-
-
-/* Define to the flags needed for the .section .eh_frame directive. */
-#define EH_FRAME_FLAGS "a"
-
 /* Cannot use PROT_EXEC on this target, so, we revert to alternative means */
 /* #undef FFI_EXEC_TRAMPOLINE_TABLE */
 /* Define this if you want to enable pax emulated trampolines (experimental)
    */
 /* #undef FFI_MMAP_EXEC_EMUTRAMP_PAX */
-
 /* Cannot use malloc on this target, so, we revert to alternative means */
 /* #undef FFI_MMAP_EXEC_WRIT */
 
 
-/* Define if your compiler supports pointer authentication. */
-/* #undef HAVE_ARM64E_PTRAUTH */
+
+/* Define to the flags needed for the .section .eh_frame directive. */
+#define EH_FRAME_FLAGS "a"
+/* Define if .eh_frame sections should be read-only. */
+#define HAVE_RO_EH_FRAME 1
 /* Define if your assembler supports .cfi_* directives. */
 #define HAVE_AS_CFI_PSEUDO_OP 1
 
 
+/* Define if your compiler supports pointer authentication. */
+/* #undef HAVE_ARM64E_PTRAUTH */
 /* Define if the compiler uses zarch features. */
 /* #undef HAVE_AS_S390_ZARCH */
-
-
 /* Define if your assembler supports unwind section type. */
 #define HAVE_AS_X86_64_UNWIND_SECTION_TYPE 1
-
 /* Define if your assembler supports PC relative relocs. */
 #define HAVE_AS_X86_PCREL 1
 
@@ -161,8 +52,6 @@
 
 
 
-/* Define if .eh_frame sections should be read-only. */
-#define HAVE_RO_EH_FRAME 1
 
 
 
