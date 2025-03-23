9563c8ac59f671243c3a8c007a1668613f3e574b
gaomingyu <874774259@qq.com>
2025-03-24 01:04:24 +0800

    build

--- build-msvc.bat
+++
@@ -1,6 +0,0 @@
-if not defined VCToolsVersion (
-  call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" amd64
-)
-cd %~dp0libffi
-gyp libffi.gyp --depth=. --generator-output=../gyp-build-ffi --format=msvs -Gmsvs_version=2022
-msbuild ../gyp-build-ffi/libffi.sln -bl:../gyp-build-ffi/libffi.binlog -p:Configuration=Default -p:Platform=Win32
