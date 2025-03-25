6a33e89fae9ac851b17ccf3111cc519e01ab16d3
gaomingyu <874774259@qq.com>
2025-03-25 16:00:41 +0800

    nodejs-ffi

--- run-build.bat
+++ run-build.bat
@@ -1,5 +1,3 @@
-if not defined VCToolsVersion (
-  call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" amd64
-)
-gyp libffi.gyp --depth=. --generator-output=../gyp-build-ffi --format=msvs -Gmsvs_version=2022
-msbuild ../gyp-build-ffi/libffi.sln -bl:../gyp-build-ffi/libffi.binlog -p:Configuration=Default -p:Platform=Win32
+del /F /Q /S "..\node\deps\libffi"
+xcopy /S /Y ".\src" "..\node"
+cmd /C "..\node\vcbuild.bat debug ffi"
--- run-node-build.bat
+++
@@ -1,3 +0,0 @@
-del /F /Q /S "..\node\deps\libffi"
-xcopy /S /Y ".\src" "..\node"
-cmd /C "..\node\vcbuild.bat debug ffi"
