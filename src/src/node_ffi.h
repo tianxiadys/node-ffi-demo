b4e9b2440cdc1d6417a33f8b3b9808fc99fa45b3
gaomingyu <874774259@qq.com>
2025-03-27 16:45:38 +0800

    node-ffi

--- src/src/node_ffi.h
+++ src/src/node_ffi.h
@@ -2,98 +2,57 @@
 #define SRC_NODE_FFI_H_
 
 #if defined(NODE_WANT_INTERNALS) && NODE_WANT_INTERNALS
-
 #include "ffi.h"
-#include "node_binding.h"
 
 namespace node::ffi
 {
-    class FFIFunction
-    {
-        ffi_cif ffiCif{};
-        std::unique_ptr<ffi_type*[]> typeArr;
+    using binding::DLib;
+    using v8::Context;
+    using v8::External;
+    using v8::FunctionCallbackInfo;
+    using v8::Isolate;
+    using v8::Local;
+    using v8::Object;
+    using v8::Value;
 
+    class FFIDefinition
+    {
     public:
-        static ffi_type* parseType(char code);
-        explicit FFIFunction(const char* defStr);
+        explicit FFIDefinition(const char* defStr);
+
+    protected:
+        static constexpr auto ABI = FFI_DEFAULT_ABI;
+        ffi_cif cif{};
+        std::unique_ptr<ffi_type*[]> types;
     };
 
-    class FFILibrary
+    class FFIFunction : public FFIDefinition
     {
+    public:
+        explicit FFIFunction(const char* defStr, void* address);
+        void setParam(int i, const void* ptr);
+        void doInvoke(ffi_raw* result);
+
+    protected:
+        void (*invoker)(){};
+        std::unique_ptr<ffi_raw[]> datas;
+        std::unique_ptr<void*[]> args;
     };
 
-    // class FfiLibrary : public BaseObject
-    // {
-    // public:
-    //     void GetAddress();
-    // };
-    //
-    // class FfiFunction : public BaseObject
-    // {
-    // public:
-    //     void DownCall();
-    // };
-    //
-    // class FfiCallback : public BaseObject
-    // {
-    // public:
-    //     void UpCall();
-    // };
-    //
-    //
-    // namespace ffi
-    // {
-    //     class FfiSignature : public BaseObject
-    //     {
-    //     public:
-    //         FfiSignature(Environment* env,
-    //                      Local<Object> object,
-    //                      Local<BigInt> fn,
-    //                      Local<BigInt> ret_type,
-    //                      Local<Array> arg_types);
-    //         static void New(const v8::FunctionCallbackInfo<v8::Value>& args);
-    //         SET_SELF_SIZE(FfiSignature)
-    //         SET_MEMORY_INFO_NAME(FfiSignature)
-    //         SET_NO_MEMORY_INFO()
-    //
-    //         ffi_cif cif_;
-    //         std::vector<ffi_type*> argv_types_;
-    //         void (*fn_)();
-    //     };
-    //
-    //     class FfiBindingData : public BaseObject
-    //     {
-    //     public:
-    //         FfiBindingData(Realm* realm, Local<Object> wrap) : BaseObject(realm, wrap)
-    //         {
-    //         }
-    //
-    //         binding::DLib* GetLibrary(std::string fname);
-    //
-    //         SET_BINDING_ID(ffi_binding_data)
-    //
-    //         void* call_buffer;
-    //
-    //         SET_SELF_SIZE(FfiBindingData)
-    //         SET_MEMORY_INFO_NAME(FfiBindingData)
-    //         SET_NO_MEMORY_INFO()
-    //
-    //     private:
-    //         std::map<std::string, std::unique_ptr<binding::DLib>> libraries_;
-    //     };
-    //
-    //     void MakeCall(const v8::FunctionCallbackInfo<Value>& args);
-    //     void AddSignature(const v8::FunctionCallbackInfo<Value>& args);
-    //     void SetCallBuffer(const v8::FunctionCallbackInfo<Value>& args);
-    //     void GetLibrary(const v8::FunctionCallbackInfo<Value>& args);
-    //     void GetSymbol(const v8::FunctionCallbackInfo<Value>& args);
-    //
-    //     void Initialize(v8::Local<v8::Object> target,
-    //                     v8::Local<v8::Value> unused,
-    //                     v8::Local<v8::Context> context,
-    //                     void* priv);
-    // } // namespace ffi
-} // namespace node::ffi
+    class FFICallback : public FFIDefinition
+    {
+    public:
+        explicit FFICallback(const char* defStr);
+        Local<External> getAddress(Isolate* isolate) const;
+        ~FFICallback();
 
-#endif  // defined(NODE_WANT_INTERNALS) && NODE_WANT_INTERNALS
-#endif  // SRC_NODE_FFI_H_
+    protected:
+        static constexpr auto FCS = sizeof(ffi_closure);
+        static void RawCallback(ffi_cif*, void*, void**, void*);
+        ffi_closure* pfc{};
+        void* address{};
+    };
+}
+
+#endif // defined(NODE_WANT_INTERNALS) && NODE_WANT_INTERNALS
+#endif // SRC_NODE_FFI_H_
