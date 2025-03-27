57c631c00c8d17db60e3da43999a3300f936ef9a
gaomingyu <874774259@qq.com>
2025-03-27 16:34:58 +0800

    FFIDefinition

--- debug/node_debug1.cc
+++ debug/node_debug1.cc
@@ -1,10 +1,18 @@
 #include "node_ffi.h"
 #include "windows.h"
 
+BOOL enumProc(HWND hWnd, LPARAM lParam)
+{
+    wprintf(L"wnd:%p\n", hWnd);
+    return 1;
+}
+
 int main()
 {
-    const auto a = new node::ffi::FFIDefinition("L");
-    const auto b = new node::ffi::FFICallback("L");
-    delete a;
-    delete b;
+    node::ffi::FFIFunction a("ipL", EnumWindows);
+    node::ffi::FFICallback b("ipL");
+    void* ptr = b.address;
+    a.setParam(0, &ptr);
+    ffi_raw ret;
+    a.doInvoke(&ret);
 }
